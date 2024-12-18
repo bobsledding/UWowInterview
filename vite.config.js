94c8a262a98f69d34b1760a15e3e493c922fa290
bobsledding <bobsledding@hotmail.com>
2024-12-18 21:25:21 +0800

    chore: init project

---
+++ vite.config.js
@@ -0,0 +1,33 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
+
+export default defineConfig({
+    plugins: [
+        laravel([
+            'resources/js/app.js',
+        ]),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
+    ],
+    resolve: {
+        alias: {
+            vue: 'vue/dist/vue.esm-bundler.js',
+        },
+    },
+    server: {
+        host: '0.0.0.0',
+        hmr: {
+            host: 'localhost'
+        },
+        watch: {
+            usePolling: true,
+        },
+    },
+});
